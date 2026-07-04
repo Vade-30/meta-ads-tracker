a513bb0811787df3f5be9dfad565d8f05862e47a
Vade-30 <piquero.benjie.g@gmail.com>
2026-07-04 13:24:32 +0800

    test: force search to include spam folder using in:anywhere

--- check_gmail.py
+++ check_gmail.py
@@ -64,7 +64,7 @@ logger = logging.getLogger("check_gmail")
 GMAIL_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 SENDER_FILTER = "2017001293@pampangastateu.edu.ph"
 SUBJECT_FILTER = '"A purchase was made"'
-SEARCH_QUERY = f'from:{SENDER_FILTER} subject:{SUBJECT_FILTER}'
+SEARCH_QUERY = f'from:{SENDER_FILTER} subject:{SUBJECT_FILTER} in:anywhere'
 FACEBOOK_MERCHANT_PATTERN = re.compile(r"facebook", re.IGNORECASE)
 
 # State keys
